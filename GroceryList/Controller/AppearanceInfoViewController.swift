cba9dc3cd5e50e237fd5377c54071c82a350dfed
GGunta <msdoors@gmail.com>
2021-08-05 21:33:16 +0300

    2 errors (override and delete list)
    Need to fix 2 errors:
    -already overridden viewDidLoad in AppearanceInfoViewContrl
    -in GroceryTableViewContrl func deleteAllItems is not ok

---
+++ GroceryList/Controller/AppearanceInfoViewController.swift
@@ -0,0 +1,100 @@
+//
+//  AppearanceInfoViewController.swift
+//  GroceryList
+//
+//  Created by gunta.golde on 05/08/2021.
+//
+
+import UIKit
+
+class AppearanceInfoViewController: UIViewController {
+    
+    
+    @IBOutlet weak var appInfoLabel: UILabel!
+    
+    var infoText = ""
+       
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if !infoText.isEmpty {
+            appInfoLabel.text = infoText
+        }
+    }
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLabelText()
+    }
+       
+    
+    @IBAction func openSettingsTapped(_ sender: Any) {
+        openSettings()
+    }
+    
+    func setLabelText(){
+        var text = "Unable to specify UI style"
+        if self.traitCollection.userInterfaceStyle == .dark{
+            text = "Dark Mode is On\nGo to Settings if you would like\nto change to Light Mode."
+        }else{
+            text = "Light Mode is On\nGo to Settings if you would like\nto change to Dark Mode."
+        }
+        textLabel.text = text
+    }
+    
+    func openSettings(){
+        guard let settingURL = URL(string: UIApplication.openSettingsURLString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(settingURL){
+            UIApplication.shared.open(settingURL, options: [:]) { success in
+                print("sucess: ", success)
+            }
+        }
+    }
+}
+
+extension AppearanceInfoViewController {
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setLabelText()
+    }
+}
+
+/*
+// @IBOutlet weak var textLabel: UILabel!
+ 
+ override func viewDidLoad() {
+     super.viewDidLoad()
+     setLabelText()
+ }
+ 
+ 
+// @IBAction func openSettingsTapped(_ sender: Any) {
+     openSettings()
+ }
+ 
+ func setLabelText(){
+     var text = "Unable to specify UI style"
+     if self.traitCollection.userInterfaceStyle == .dark{
+         text = "Dark Mode is On\nGo to Settings if you would like\nto change to Light Mode."
+     }else{
+         text = "Light Mode is On\nGo to Settings if you would like\nto change to Dark Mode."
+     }
+     textLabel.text = text
+ }
+ 
+ func openSettings(){
+     guard let settingURL = URL(string: UIApplication.openSettingsURLString) else {
+         return
+     }
+     
+     if UIApplication.shared.canOpenURL(settingURL){
+         UIApplication.shared.open(settingURL, options: [:]) { success in
+             print("sucess: ", success)
+         }
+     }
+ }
+}*/
